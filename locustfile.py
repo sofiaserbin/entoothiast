3bd8b9707079e58ad8a94a441641a95e3aadbb88
Sofia Serbina <serbina@student.chalmers.se>
2023-12-06 13:39:25 +0300

    Add more tasks #15

--- locustfile.py
+++ locustfile.py
@@ -1,5 +1,7 @@
 from locust import HttpUser, task, between
 from faker import Faker
+import random
+from datetime import datetime, timedelta
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
@@ -11,13 +13,13 @@ class QuickstartUser(HttpUser):
         fake_username = self.fake.user_name()
         fake_password = self.fake.password()
         fake_name = self.fake.name()
-        fake_role = "dentist"
+        
 
         register_payload = {
             "username": fake_username,
             "password": fake_password,
             "name": fake_name,
-            "role": fake_role
+            "role": "patient"
         } 
 
         self.client.post("/v1/users/register", json=register_payload)
@@ -52,7 +54,27 @@ class QuickstartUser(HttpUser):
 
     @task 
     def get_all_dentists(self):
-        self.client.get("/v1/detists")
+        self.client.get("/v1/dentists")
+
+    @task 
+    def create_new_timeslot(self):
+        random_dentistId = random.randint(31, 36)
+        start_time = datetime.now() + timedelta(days=random.randint(1, 30))
+        end_time = start_time + timedelta(hours=random.randint(1, 6))
+    
+        newTimeslot_payload = {
+        "start_time": start_time.strftime("%Y-%m-%d %H:%M:%S"),
+        "end_time": end_time.strftime("%Y-%m-%d %H:%M:%S")
+         }
+        self.client.post(f'/v1/dentists/{random_dentistId}/timeslots', json = newTimeslot_payload)
+
+    @task 
+    def rate_dentist(self):
+        random_dentistId = random.randint(31, 36)
+        rating_payload = {
+            "rating": random.randint(1, 5)
+        }
+        self.client.post(f'/v1/dentists/{random_dentistId}/ratings', json = rating_payload)
 
     
 
