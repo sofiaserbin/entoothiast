1783667803e96ac4dee72d357649f094ca4769bf
Michael Koenig <koenigmichael2002@gmail.com>
2023-12-13 11:24:00 +0100

    Add initial users for testing #16

---
+++ initial-data.sql
@@ -0,0 +1,12 @@
+-- NOTE: all passwords are "test" (since those are only meant for testing)
+
+INSERT INTO "user" ("name", "username", "pw_hash", "role") VALUES ('Patient 1', 'patient1', '$2b$10$jMW33.ViVG8r9Jv7nsbPhu2ItagSF4r8e2r5iqZClgq/e7XEk5Pwy', 'patient');
+INSERT INTO "user" ("name", "username", "pw_hash", "role") VALUES ('Patient 2', 'patient2', '$2b$10$jMW33.ViVG8r9Jv7nsbPhu2ItagSF4r8e2r5iqZClgq/e7XEk5Pwy', 'patient');
+INSERT INTO "user" ("name", "username", "pw_hash", "role") VALUES ('Patient 3', 'patient3', '$2b$10$jMW33.ViVG8r9Jv7nsbPhu2ItagSF4r8e2r5iqZClgq/e7XEk5Pwy', 'patient');
+INSERT INTO "user" ("name", "username", "pw_hash", "role") VALUES ('Patient 4', 'patient5', '$2b$10$jMW33.ViVG8r9Jv7nsbPhu2ItagSF4r8e2r5iqZClgq/e7XEk5Pwy', 'patient');
+
+INSERT INTO "user" ("name", "username", "pw_hash", "role") VALUES ('Dentist 1', 'dentist1', '$2b$10$jMW33.ViVG8r9Jv7nsbPhu2ItagSF4r8e2r5iqZClgq/e7XEk5Pwy', 'dentist');
+INSERT INTO "user" ("name", "username", "pw_hash", "role") VALUES ('Dentist 2', 'dentist2', '$2b$10$jMW33.ViVG8r9Jv7nsbPhu2ItagSF4r8e2r5iqZClgq/e7XEk5Pwy', 'dentist');
+INSERT INTO "user" ("name", "username", "pw_hash", "role") VALUES ('Dentist 3', 'dentist3', '$2b$10$jMW33.ViVG8r9Jv7nsbPhu2ItagSF4r8e2r5iqZClgq/e7XEk5Pwy', 'dentist');
+INSERT INTO "user" ("name", "username", "pw_hash", "role") VALUES ('Dentist 4', 'dentist5', '$2b$10$jMW33.ViVG8r9Jv7nsbPhu2ItagSF4r8e2r5iqZClgq/e7XEk5Pwy', 'dentist');
+
